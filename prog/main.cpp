57393784d0d6f34f7e7fe6c6e828326d2513a8f7
Connor Sweeney <connor.sweeney1234@gmail.com>
2023-12-26 17:15:02 +0000

    Improved variable names and fully commented code

--- prog/main.cpp
+++ prog/main.cpp
@@ -11,19 +11,19 @@ int gScreenHeight = 720;
 SDL_Window* gGraphicsApplicationWindow = nullptr; 
 SDL_GLContext gOpenGLContext = nullptr;
 
-bool gQuit = false;
-
 GLuint gVertexArrayObject = 0;
 GLuint gVertexBufferObject = 0;
 GLuint gIndexBufferObject = 0;
 GLuint gGraphicsPipelineShaderProgram = 0;
 
-void GLClearAllErrors()
+bool gQuit = false;
+
+void ClearAllGLErrors() // Clears any errors that might have been generated previously
 {
     while (glGetError() != GL_NO_ERROR){ }
 }
 
-bool GLCheckErrorStatus(const char* functionName, const char* fileName, int line)
+bool CheckGLErrorStatus(const char* functionName, const char* fileName, int line) // Checks for errors and displays them
 {
     while (GLenum error = glGetError())
     {
@@ -37,9 +37,128 @@ bool GLCheckErrorStatus(const char* functionName, const char* fileName, int line
     return false;
 }
 
-#define GLCheck(function) GLClearAllErrors(); function; GLCheckErrorStatus(#function, __FILE__, __LINE__);
+#define CheckGL(function) ClearAllGLErrors(); function; CheckGLErrorStatus(#function, __FILE__, __LINE__); // Macro for use in finding and displaying errors
 
-std::string LoadShaderAsString(const std::string& fileName)
+void GetOpenGLVersionInfo() // Displays information about the OpenGL version
+{
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "Version:" << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl; 
+}
+
+void InitializeProgram() // Initializes SDL2 4.1, OpenGL, and GLAD
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        std::cout << "SDL2 could not be initialized!" << std::endl;
+        exit(1);
+    }
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // Double buffering enabled
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);  // 24 layers of depth
+
+    gGraphicsApplicationWindow = SDL_CreateWindow(
+                                                  "SDLGL Test",                // Window title
+                                                  100, 100,                    // Window top left corner coordinates
+                                                  gScreenWidth, gScreenHeight, // Window width and height
+                                                  SDL_WINDOW_OPENGL            // Window type
+                                                 );
+    if (gGraphicsApplicationWindow == nullptr)
+    {
+        std::cout << "SDL_Window could not be created!" << std::endl;
+        exit(1);
+    }
+
+    gOpenGLContext = SDL_GL_CreateContext(gGraphicsApplicationWindow); // Creates an OpenGL context associated with the window
+    if (gOpenGLContext == nullptr)
+    {
+        std::cout << "OpenGl context could not be created!" << std::endl;
+        exit(1);
+    }
+
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) // Loads OpenGL functions using GLAD
+    {
+        std::cout << "Glad could not be initialized!" << std::endl;
+        exit(1);
+    }
+
+    GetOpenGLVersionInfo();
+}
+
+void VertexSpecification() // Creates a vertex array object, a vertex buffer object, and an index buffer object
+{
+    glGenVertexArrays(1, &gVertexArrayObject);
+    glBindVertexArray(gVertexArrayObject);
+
+    const std::vector<GLfloat> vertexData
+    {
+        // Vertex 0
+        -0.5f, -0.5f, 0.0f, // Bottom left vertex position
+        1.0f, 0.0f, 0.0f,   // Bottom left vertex color
+        // Vertex 1
+        0.5f, -0.5f, 0.0f,  // Bottom right vertex position
+        0.0f, 1.0f, 0.0f,   // Bottom right vertex color
+        // Vertex 2
+        -0.5f, 0.5f, 0.0f,  // Top left vertex position
+        0.0f, 0.0f, 1.0f,   // Top left vertex color
+        // Vertex 3
+        0.5f, 0.5f, 0.0f,   // Top right vertex position
+        1.0f, 0.0f, 0.0f    // Top right vertex color
+    };
+
+    glGenBuffers(1, &gVertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, gVertexBufferObject);
+    glBufferData(
+                 GL_ARRAY_BUFFER,                     // Type of buffer to target
+                 vertexData.size() * sizeof(GLfloat), // Size of the buffer in bytes
+                 vertexData.data(),                   // The data to be used
+                 GL_STATIC_DRAW                       // How the data will be used
+                );
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(                    // Vertix attribute pointer for the vertex position
+                          0,                  // Index of the vertex attribute pointer
+                          3,                  // Number of components per vertex attribute
+                          GL_FLOAT,           // Type of data of each component
+                          GL_FALSE,           // Whether the data is normalized
+                          sizeof(GL_FLOAT)*6, // Stride (the space between consecutive vertex attributes)
+                          (GLvoid*)0          // Offset of the first component
+                         ); 
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(                              // Vertex attribute pointer for the vertex color
+                          1,                            // Index of the vertex attribute pointer
+                          3,                            // Number of components per vertex attribute
+                          GL_FLOAT,                     // Type of data of each component
+                          GL_FALSE,                     // Whether the data is normalized
+                          sizeof(GL_FLOAT)*6,           // Stride (the space between consecutive vertex attributes)
+                          (GLvoid*)(sizeof(GL_FLOAT)*3) // Offset of the first component
+                         );
+
+    const std::vector<GLuint> indexData
+    {
+        0, 1, 2, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    glGenBuffers(1, &gIndexBufferObject);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIndexBufferObject);
+    glBufferData(
+                 GL_ELEMENT_ARRAY_BUFFER,           // Type of buffer to target
+                 indexData.size() * sizeof(GLuint), // Size of the buffer in bytes
+                 indexData.data(),                  // The data to be used
+                 GL_STATIC_DRAW                     // How the data will be used
+                );
+
+    glBindVertexArray(0);
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+}
+
+std::string LoadShaderAsString(const std::string& fileName) // Loads a shader from a file and returns it as a string
 {
     std::string result;
 
@@ -62,97 +181,7 @@ std::string LoadShaderAsString(const std::string& fileName)
     return result;
 }
 
-void GetOpenGLVersionInfo()
-{
-    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-    std::cout << "Version:" << glGetString(GL_VERSION) << std::endl;
-    std::cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl; 
-}
-
-void InitializeProgram()
-{
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-    {
-        std::cout << "SDL2 could not be initialized!" << std::endl;
-        exit(1);
-    }
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-    gGraphicsApplicationWindow = SDL_CreateWindow("SDLGL Test", 100, 100, gScreenWidth, gScreenHeight, SDL_WINDOW_OPENGL);
-    if (gGraphicsApplicationWindow == nullptr)
-    {
-        std::cout << "SDL_Window could not be created!" << std::endl;
-        exit(1);
-    }
-
-    gOpenGLContext = SDL_GL_CreateContext(gGraphicsApplicationWindow);
-    if (gOpenGLContext == nullptr)
-    {
-        std::cout << "OpenGl context could not be created!" << std::endl;
-        exit(1);
-    }
-
-    if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
-    {
-        std::cout << "Glad could not be initialized!" << std::endl;
-        exit(1);
-    }
-
-    GetOpenGLVersionInfo();
-}
-
-void VertexSpecification()
-{
-    glGenVertexArrays(1, &gVertexArrayObject);
-    glBindVertexArray(gVertexArrayObject);  
-
-    const std::vector<GLfloat> vertexData
-    {
-        // vertex 0
-        -0.5f, -0.5f, 0.0f, // bottom left vertex position
-        1.0f, 0.0f, 0.0f,   // bottom left vertex color
-        // vertex 1
-        0.5f, -0.5f, 0.0f,  // bottom right vertex position
-        0.0f, 1.0f, 0.0f,   // bottom right vertex color
-        // vertex 2
-        -0.5f, 0.5f, 0.0f,  // top left vertex position
-        0.0f, 0.0f, 1.0f,   // top left vertex color
-        // vertex 3
-        0.5f, 0.5f, 0.0f,   // top right vertex position
-        1.0f, 0.0f, 0.0f    // top right vertex color
-    };
-
-    glGenBuffers(1, &gVertexBufferObject);
-    glBindBuffer(GL_ARRAY_BUFFER, gVertexBufferObject);
-    glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(GLfloat), vertexData.data(), GL_STATIC_DRAW);
-    
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT)*6, (GLvoid*)0); 
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT)*6, (GLvoid*)(sizeof(GL_FLOAT)*3));
-
-    const std::vector<GLuint> indexData
-    {
-        0, 1, 2, // first triangle
-        1, 2, 3  // second triangle
-    };
-
-    glGenBuffers(1, &gIndexBufferObject);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIndexBufferObject);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexData.size() * sizeof(GLuint), indexData.data(), GL_STATIC_DRAW);
-
-    glBindVertexArray(0);
-    glDisableVertexAttribArray(0);
-    glDisableVertexAttribArray(1);
-}
-
-GLuint CompileShader(GLuint type, const std::string& shaderSource)
+GLuint CompileShader(GLuint type, const std::string& shaderSource) // Compiles a shader and returns its ID
 {
     GLuint shaderObject;
 
@@ -166,46 +195,51 @@ GLuint CompileShader(GLuint type, const std::string& shaderSource)
     }
     
     const char* source = shaderSource.c_str();
-    glShaderSource(shaderObject, 1, &source, nullptr);
+    glShaderSource(
+                   shaderObject, // Destination for the shader source
+                   1,            // Number of elements in the string and length arrays
+                   &source,      // The shader source code as a string
+                   nullptr       // Array of string lengths
+                  );
     glCompileShader(shaderObject);
     
     int result;
-    glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &result); // Gets the compile status of the shader
 
-    if (result == GL_FALSE)
+    if (result == GL_FALSE) // If the shader failed to compile
     {
         int length;
-        glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &length);
-        char* errorMessages = new char[length];
-        glGetShaderInfoLog(shaderObject, length, &length, errorMessages);
+        glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &length);         // Gets the length of the error message
+        char* errorMessages = new char[length];                           // Creates a new char array with the length of the error message
+        glGetShaderInfoLog(shaderObject, length, &length, errorMessages); // Gets the error message
 
         if (type == GL_VERTEX_SHADER)
         {
-            std::cout << "GL_VERTEX_SHADER compilation failed!\n" << errorMessages << std::endl;
+            std::cout << "GL_VERTEX_SHADER compilation failed!\n" << errorMessages << std::endl;   // Displays the vertex shader error message
         }
         else if (type == GL_FRAGMENT_SHADER)
         {
-            std::cout << "GL_FRAGMENT_SHADER compilation failed!\n" << errorMessages << std::endl;
+            std::cout << "GL_FRAGMENT_SHADER compilation failed!\n" << errorMessages << std::endl; // Displays the fragment shader error message
         }
 
-        delete[] errorMessages;
-        glDeleteShader(shaderObject);
+        delete[] errorMessages;       // Deletes the error message char array
+        glDeleteShader(shaderObject); // Deletes the shader object
 
         return 0;
     }
 
-    return shaderObject;
+    return shaderObject; // Upon successful compilation, returns the shader object
 }
 
-GLuint CreateShaderProgram(const std::string& vertexShaderSource, const std::string& fragmentShaderSource)
+GLuint CreateShaderProgram(const std::string& vertexShaderSource, const std::string& fragmentShaderSource) // Creates a shader program and returns its ID
 {
     GLuint programObject = glCreateProgram();
 
-    GLuint myVertexShader = CompileShader(GL_VERTEX_SHADER, vertexShaderSource);
-    GLuint myFragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+    GLuint vertexShader = CompileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
 
-    glAttachShader(programObject, myVertexShader);
-    glAttachShader(programObject, myFragmentShader);
+    glAttachShader(programObject, vertexShader);
+    glAttachShader(programObject, fragmentShader);
     glLinkProgram(programObject);
 
     glValidateProgram(programObject);
@@ -213,7 +247,7 @@ GLuint CreateShaderProgram(const std::string& vertexShaderSource, const std::str
     return programObject;
 }
 
-void CreateGraphicsPipeline()
+void CreateGraphicsPipeline() // At minimum, a graphics pipeline consists of a vertex shader and a fragment shader
 {
     std::string vertexShaderSource = LoadShaderAsString("prog/shaders/vertexShader.glsl");
     std::string fragmentShaderSource = LoadShaderAsString("prog/shaders/fragmentShader.glsl");
@@ -221,7 +255,7 @@ void CreateGraphicsPipeline()
     gGraphicsPipelineShaderProgram = CreateShaderProgram(vertexShaderSource, fragmentShaderSource); 
 }
 
-void Input()
+void Input() // Handles input events
 {
     SDL_Event e;
 
@@ -237,13 +271,18 @@ void Input()
 
 void PreDraw()
 {
-    glDisable(GL_DEPTH_TEST);
-    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST); // Depth testing disabled
+    glDisable(GL_CULL_FACE);  // Face culling disabled
 
-    glViewport(0, 0, gScreenWidth, gScreenHeight);
-    glClearColor(0.2f, 0.2f, 0.2f, 1.f);
+    glViewport(
+               0,            // X position of the lower left corner of the viewport rectangle
+               0,            // Y position of the lower left corner of the viewport rectangle
+               gScreenWidth, // Width of the viewport rectangle
+               gScreenHeight // Height of the viewport rectangle
+              );
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
-    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT); // Resets the depth and color buffers to the values set above
 
     glUseProgram(gGraphicsPipelineShaderProgram);
 }
@@ -253,12 +292,17 @@ void Draw()
     glBindVertexArray(gVertexArrayObject);
     glBindBuffer(GL_ARRAY_BUFFER, gVertexBufferObject);
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (GLvoid*)0);
+    glDrawElements(
+                   GL_TRIANGLES,    // Type of primitive to draw
+                   6,               // Number of vertexes to be rendered
+                   GL_UNSIGNED_INT, // Type of the vertexes values
+                   (GLvoid*)0       // Offset of the first index
+                  );
 
     glUseProgram(0);
 }
 
-void MainLoop()
+void MainLoop() // The main loop of the program
 {
     while (!gQuit)
     {
@@ -272,10 +316,12 @@ void MainLoop()
     }
 }
 
-void CleanUp()
+void CleanUp() // Cleans up the program
 {
+    glDeleteProgram(gGraphicsPipelineShaderProgram);
+    SDL_GL_DeleteContext(gOpenGLContext);
     SDL_DestroyWindow(gGraphicsApplicationWindow);
-    SDL_Quit(); 
+    SDL_Quit();
 }
 
 int main(int argc, char* argv[])
