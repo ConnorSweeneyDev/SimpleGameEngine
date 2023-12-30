8432f53e68bcd89f0e7a1f0ebeefdd1f0529eb99
Connor Sweeney <connor.sweeney1234@gmail.com>
2023-12-30 01:00:57 +0000

    Improved Camera class

--- prog/Camera.cpp
+++ prog/Camera.cpp
@@ -2,10 +2,7 @@
 
 Camera camera;
 
-glm::mat4 Camera::getViewMatrix() const
-{
-    return glm::lookAt(position, position + direction, up);
-}
+glm::mat4 Camera::getViewMatrix() const { return glm::lookAt(position, position + direction, up); }
 
 void Camera::moveUp(float speed) { position.y += speed; }       // Movements are on the global axes, since the camera cannot rotate
 void Camera::moveDown(float speed) { position.y -= speed; }
@@ -23,5 +20,7 @@ void Camera::resetPosition()
 
 void Camera::init()
 {
-    resetPosition();
+    position = glm::vec3(0.0f, 0.0f, 3.0f);
+    direction = glm::vec3(0.0f, 0.0f, -1.0f);
+    up = glm::vec3(0.0f, 1.0f, 0.0f);
 }
