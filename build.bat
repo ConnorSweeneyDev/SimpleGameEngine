3510af68df563013fc31e24e90593e553d09a9d5
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-01-16 21:06:00 +0000

    Updated mingw and gcc to use C++23

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-g++ -g -std=c++17 prog/*.cpp src/glad.c -Iprog/include -Isrc/include -Isrc/include/glad -Isrc/include/glm -Isrc/include/KHR -Isrc/include/SDL2 -Lsrc/lib -Wall -lmingw32 -lSDL2main -lSDL2 -o SDLGLTest.exe
+g++ -g -std=c++23 prog/*.cpp src/glad.c -Iprog/include -Isrc/include -Isrc/include/glad -Isrc/include/glm -Isrc/include/KHR -Isrc/include/SDL2 -Lsrc/lib -Wall -lmingw32 -lSDL2main -lSDL2 -o SDLGLTest.exe
