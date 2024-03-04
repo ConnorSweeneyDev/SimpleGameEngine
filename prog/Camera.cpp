1f0b9300086f6d216c052beb33c6d011cb1ff7d4
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-03-04 00:49:01 +0000

    Removed unintended Z rotation upon combining X and Y rotation

--- prog/Camera.cpp
+++ prog/Camera.cpp
@@ -17,16 +17,8 @@ void Camera::MoveForward(float speed) { position.z -= speed * time_util.getDelta
 void Camera::MoveBackward(float speed) { position.z += speed * time_util.getDeltaTime(); }
 
 // Camera rotation is relative to itself
-void Camera::RotateXPos(float speed)
-{
-    direction = glm::rotate(direction, speed * time_util.getDeltaTime(),  glm::cross(direction, up));
-    up = glm::rotate(up, speed * time_util.getDeltaTime(), glm::cross(direction, up));
-}
-void Camera::RotateXNeg(float speed)
-{
-    direction = glm::rotate(direction, -speed * time_util.getDeltaTime(), glm::cross(direction, up));
-    up = glm::rotate(up, -speed * time_util.getDeltaTime(), glm::cross(direction, up));
-}
+void Camera::RotateXPos(float speed) { direction = glm::rotate(direction, speed * time_util.getDeltaTime(),  glm::cross(direction, up)); }
+void Camera::RotateXNeg(float speed) { direction = glm::rotate(direction, -speed * time_util.getDeltaTime(), glm::cross(direction, up)); }
 void Camera::RotateYPos(float speed) { direction = glm::rotate(direction, speed * time_util.getDeltaTime(), up); }
 void Camera::RotateYNeg(float speed) { direction = glm::rotate(direction, -speed * time_util.getDeltaTime(), up); }
 void Camera::RotateZPos(float speed) { up = glm::rotate(glm::mat4(1.0f), speed * time_util.getDeltaTime(), direction) * glm::vec4(up, 1.0f); }
