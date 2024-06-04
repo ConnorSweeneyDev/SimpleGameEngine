1aff2d8a2e388e71170bf178ee20a5672e7a33d5
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-06-04 05:30:57 +0100

    Temporary deletion

--- binary/linux/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-package="libsdl2-dev"
-if !(dpkg-query -W -f='${Status}' $package 2>/dev/null | grep -q "install ok installed"); then
-    echo "$package is not installed. Installing..."
-    sudo apt install -y $package
-fi
-
-./binary/linux/3DGameEngine
