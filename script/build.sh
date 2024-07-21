660e8a7c50461968c2c5f0edb3fd75d96a037ed2
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-07-21 20:10:42 +0100

    Better incremental resource builds for linux

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make -j $(python3 -c 'import multiprocessing as mp; print(int(mp.cpu_count() * 1.5))')
+make external -j $(python3 -c 'import multiprocessing as mp; print(int(mp.cpu_count() * 1.5))') && make main -j $(python3 -c 'import multiprocessing as mp; print(int(mp.cpu_count() * 1.5))')
