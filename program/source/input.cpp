7bda024b5e78c7a649ed0adf59113518f64c613a
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-08-14 00:47:59 +0100

    Naming convention

--- program/source/input.cpp
+++ program/source/input.cpp
@@ -55,9 +55,9 @@ namespace cse::input
         object::Transform_data{glm::Vec3(0.0f, 0.0f, 0.0f), glm::Vec3(0.0f, 0.0f, 0.f),
                                glm::Vec3(1.0f, 1.0f, 1.0f)});
     }
-    auto item_1 = object::render.get_by_name<object::Item>("Item 1");
-    if (item_1)
-      if (key_state[SDL_SCANCODE_0]) object::render.remove(item_1);
+    auto item1 = object::render.get_by_name<object::Item>("Item 1");
+    if (item1)
+      if (key_state[SDL_SCANCODE_0]) object::render.remove(item1);
 
     if (key_state[SDL_SCANCODE_7])
     {
@@ -67,32 +67,32 @@ namespace cse::input
         object::Transform_data{glm::Vec3(0.0f, 0.0f, 0.0f), glm::Vec3(0.0f, 0.0f, 0.f),
                                glm::Vec3(1.0f, 1.0f, 1.0f)});
     }
-    auto item_2 = object::render.get_by_name<object::Item>("Item 2");
-    if (item_2)
-      if (key_state[SDL_SCANCODE_8]) object::render.remove(item_2);
+    auto item2 = object::render.get_by_name<object::Item>("Item 2");
+    if (item2)
+      if (key_state[SDL_SCANCODE_8]) object::render.remove(item2);
 
-    auto player_1 = object::render.get_by_name<object::Player>("Player 1");
-    if (player_1)
+    auto player1 = object::render.get_by_name<object::Player>("Player 1");
+    if (player1)
     {
-      if (key_state[SDL_SCANCODE_D]) player_1->move_right();
-      if (key_state[SDL_SCANCODE_A]) player_1->move_left();
-      if (key_state[SDL_SCANCODE_W]) player_1->move_up();
-      if (key_state[SDL_SCANCODE_S]) player_1->move_down();
-      if (key_state[SDL_SCANCODE_E]) player_1->move_forward();
-      if (key_state[SDL_SCANCODE_Q]) player_1->move_backward();
-      if (key_state[SDL_SCANCODE_SPACE]) player_1->reset_transform();
+      if (key_state[SDL_SCANCODE_D]) player1->move_right();
+      if (key_state[SDL_SCANCODE_A]) player1->move_left();
+      if (key_state[SDL_SCANCODE_W]) player1->move_up();
+      if (key_state[SDL_SCANCODE_S]) player1->move_down();
+      if (key_state[SDL_SCANCODE_E]) player1->move_forward();
+      if (key_state[SDL_SCANCODE_Q]) player1->move_backward();
+      if (key_state[SDL_SCANCODE_SPACE]) player1->reset_transform();
     }
 
-    auto player_2 = object::render.get_by_name<object::Player>("Player 2");
-    if (player_2)
+    auto player2 = object::render.get_by_name<object::Player>("Player 2");
+    if (player2)
     {
-      if (key_state[SDL_SCANCODE_L]) player_2->move_right();
-      if (key_state[SDL_SCANCODE_J]) player_2->move_left();
-      if (key_state[SDL_SCANCODE_I]) player_2->move_up();
-      if (key_state[SDL_SCANCODE_K]) player_2->move_down();
-      if (key_state[SDL_SCANCODE_O]) player_2->move_forward();
-      if (key_state[SDL_SCANCODE_U]) player_2->move_backward();
-      if (key_state[SDL_SCANCODE_SPACE]) player_2->reset_transform();
+      if (key_state[SDL_SCANCODE_L]) player2->move_right();
+      if (key_state[SDL_SCANCODE_J]) player2->move_left();
+      if (key_state[SDL_SCANCODE_I]) player2->move_up();
+      if (key_state[SDL_SCANCODE_K]) player2->move_down();
+      if (key_state[SDL_SCANCODE_O]) player2->move_forward();
+      if (key_state[SDL_SCANCODE_U]) player2->move_backward();
+      if (key_state[SDL_SCANCODE_SPACE]) player2->reset_transform();
     }
   }
 }
