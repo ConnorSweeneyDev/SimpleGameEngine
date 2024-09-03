48ca4b7c9a55d55f059f85fe676dbcd457c3acac
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-09-03 19:27:12 +0100

    Better compiler flags

--- make/flags.mk
+++ make/flags.mk
@@ -1,14 +1,14 @@
 CXX := g++
 CC := gcc
 ifeq ($(DEBUG), 1)
-  CXX_FLAGS := -g3 -MD -MP -O2 -std=c++17 -DDEBUG -D_FORTIFY_SOURCE=2 -fstack-protector-strong
-  CC_FLAGS := -g3 -MD -MP -O2 -DDEBUG -D_FORTIFY_SOURCE=2 -fstack-protector-strong
+  CXX_FLAGS := -g3 -MD -MP -O2 -std=c++17 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
+  CC_FLAGS := -g3 -MD -MP -O2 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
 else
-  CXX_FLAGS := -s -O3 -std=c++17 -DNDEBUG -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
-  CC_FLAGS := -s -O3 -DNDEBUG -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
+  CXX_FLAGS := -s -O3 -std=c++17 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
+  CC_FLAGS := -s -O3 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
 endif
 
-WARNINGS := -Wall -Wextra -Wpedantic -Wconversion -Wshadow -Wundef -Wdouble-promotion -fno-common -Wcast-qual -Wcast-align -Wfloat-equal -Wlogical-op -Wduplicated-cond -Wshift-overflow=2 -Wformat=2
+WARNINGS := -Wall -Wextra -Wpedantic -Wconversion -Wshadow -Wundef -Wdouble-promotion -Wcast-qual -Wcast-align -Wfloat-equal -Wlogical-op -Wduplicated-cond -Wshift-overflow=2 -Wformat=2
 INCLUDES := -Iprogram/include
 ifeq ($(UNAME), Windows)
   SYSTEM_INCLUDES := -isystemexternal/include -isystemexternal/include/glad -isystemexternal/include/glm -isystemexternal/include/khr -isystemexternal/include/sdl2/windows -isystemexternal/include/stb
