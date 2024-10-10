bfb5fc18c9c605787668aafc265e002d7173a484
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-10 02:36:07 +0100

    Removed python dependency for linux

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CPU_COUNT_MULTIPLIER=$(python3 -c 'import multiprocessing as mp; print(int(mp.cpu_count() * 1.5))')
-FLAGS="-s -f make/main.mk -j $CPU_COUNT_MULTIPLIER DEBUG=1"
+CPU_COUNT_MULTIPLIER=$(nproc)
+FLAGS="-s -f make/main.mk -j$CPU_COUNT_MULTIPLIER DEBUG=1"
 UTILITY="utility $FLAGS"
 PREPARE="prepare $FLAGS"
 BUILD="build $FLAGS"
