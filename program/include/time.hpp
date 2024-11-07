3b33d2743edd32e61b8cd3993742da942618003c
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-11-07 16:32:33 +0000

    Removed unnecessary timer file

--- program/include/time.hpp
+++ program/include/time.hpp
@@ -1,8 +1,29 @@
 #pragma once
 
+#include <chrono>
+#include <string>
+
+#include "rename.hpp"
+
 namespace cse::time
 {
+  constexpr sdl::Uint64 poll_rate = 144;
   void update();
   bool is_behind();
   void catchup();
+
+  class Timer
+  {
+  public:
+    Timer(const std::string &i_name);
+    ~Timer();
+
+  private:
+    long long microseconds() const;
+    float milliseconds() const;
+    float seconds() const;
+
+    std::string name;
+    std::chrono::time_point<std::chrono::high_resolution_clock> time;
+  };
 }
--- program/source/time.cpp
+++ program/source/time.cpp
@@ -1,22 +1,33 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+
 #include "rename.hpp"
 
 #include "time.hpp"
 
 namespace cse::time
 {
-  constexpr double poll_rate = 144.0;
-  double target = 1000.0 / poll_rate;
-
-  double previous = (double)sdl::get_ticks_64();
-  double lag = 0.0;
+  sdl::Uint64 target = 1000 / poll_rate;
+  sdl::Uint64 previous = sdl::get_ticks_64();
+  sdl::Uint64 lag = 0;
   void update()
   {
-    double current = (double)sdl::get_ticks_64();
-    double elapsed = current - previous;
+    sdl::Uint64 current = sdl::get_ticks_64();
+    sdl::Uint64 elapsed = current - previous;
     previous = current;
     lag += elapsed;
   }
-
   bool is_behind() { return lag >= target; }
   void catchup() { lag -= target; }
+
+  Timer::Timer(const std::string &i_name) : name(i_name) { time = std::chrono::high_resolution_clock::now(); }
+  Timer::~Timer() { std::cout << name << ": " << milliseconds() << "ms\n"; }
+  long long Timer::microseconds() const
+  {
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - time)
+      .count();
+  }
+  float Timer::milliseconds() const { return (float)microseconds() * 0.001f; }
+  float Timer::seconds() const { return milliseconds() * 0.001f; }
 }
