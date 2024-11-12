290dbb2f5d792e90f27f40f82b3e5391d89f9e2a
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-11-12 19:07:45 +0000

    Better debug flag

--- make/flags.mk
+++ make/flags.mk
@@ -1,8 +1,8 @@
 CXX := g++
 CC := gcc
 ifeq ($(DEBUG), 1)
-  CXX_FLAGS := -ggdb3 -MD -MP -O2 -std=c++17 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
-  CC_FLAGS := -ggdb3 -MD -MP -O2 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
+  CXX_FLAGS := -ggdb3 -Og -MD -MP -std=c++17 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
+  CC_FLAGS := -ggdb3 -Og -MD -MP -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
 else
   CXX_FLAGS := -s -O3 -std=c++17 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
   CC_FLAGS := -s -O3 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
