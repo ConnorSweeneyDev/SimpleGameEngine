35aeda9de919053b4f07176daf892df4e18c3199
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-04-27 20:53:11 +0100

    Added SDL_mixer, better cmakelists

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 BUILD_TYPE=Release
-cmake -S . -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+if [ ! -d "build" ]; then
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+fi
 cmake --build build --config $BUILD_TYPE
